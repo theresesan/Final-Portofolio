57490f1a5f08cfe230f28f62a686e17edead3fff
Therese Hagen <therese.kodehode@gmail.com>
2023-01-18 17:43:49 +0100

    new

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,10 +4,10 @@ function NavBar() {
   return (
     <div>
       <ul className="NavBar">
-        <li className="NavList" onClick={() => window.location.replace("/#Page2")}>
+        <li className="NavList" onClick={() => window.location.replace("./#Page2")}>
           Therese Hagen
         </li>
-        <li className="NavList" onClick={() => window.location.replace("/#Page3")}>
+        <li className="NavList" onClick={() => window.location.replace("./#Page3")}>
           Prosjekter
         </li>
       </ul>
